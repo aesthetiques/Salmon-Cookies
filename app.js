19020b3ac8c1939275040655222dda6f2661bd6e
Caleb Wells <caleb.wells1@gmail.com>
2017-02-13 12:33:20 -0800

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 'use strict';
+
+var sectionEl = document.getElementById('main-content');
+//<section id="main-content"></section>
+
+// var myUser = {
+//   fullName: userFullName,
+//   email: userEmail,
+//   handle: userHandle,
+//   followerCount: userfollowerCount,
+//   postCount: userPostCount,
+// };
+
+var userElement = document.createElement('h1'); //step one: create html element node
+userElement.setAttribute('id', 'made-an-element'); //step two: this is a method that changes an html element node
+//the first input here is the type of element that we're making, the next is the id
+//this would thus far do the followeding: <h1 id="made-an-element"></h1>
+
+sectionEl.appendChild(userElement);
