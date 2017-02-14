f8e4c21ab4237033fe83c99c067fbc4df4305e97
Caleb Wells <caleb.wells1@gmail.com>
2017-02-13 17:58:42 -0800

    randomized avg customer loop made, can't get the time range to work

--- app.js
+++ app.js
@@ -10,14 +10,38 @@
 
 // sectionEl.appendChild(userElement);
 
-var timeDay = ['6am', '7am','8am', '9am', '10am', '11 am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-var avgCookies = [onePike.avgHourly, seaTac.avgHourly, seaCenter.avgHourly, capHill.avgHourly, alki.avgHourly];
+var timeDay = ['6am', '7am', '8am', '9am', '10am', '11 am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var stores = ['1st and Pike', 'Seatac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
 
-function loopTime(){
-  for(i = 0; i < timeDay.length; i++){
-    return timeDay[i];
+var eListStart = document.getElementById('cookie-list');
+
+//Loop Store Titles to H1
+function loopStore(){
+  for(var t = 0; t < stores.length; t++){
+    return stores[t];
   };
 }
+
+//LOOP TIME OF DAY
+// function loopTime(){
+//   for(var i = 0; i < timeDay.length; i++){
+//     console.log(timeDay[i]);
+//     return timeDay[i];
+//   };
+// }
+
+//LOOP AVG COOKIES PER HR
+function loopAvgCookies(){
+  for(var j = 0; j < avgCookies.length; j++){
+    console.log(avgCookies[j]);
+  }
+};
+
+//LOOP THE HOURS OF DAY * loopAvgCookies();
+function aDay(x){
+  console.log(x.avgHourly());
+  return x.avgHourly();
+};
 
 // function randomInt(min, max){
 //   min = Math.ciel(this.minCust);
@@ -25,12 +49,14 @@ function loopTime(){
 //   return Math.floor(Math.random() * (max - min + 1 )) + this.minCust;
 // }
 
+//--STORE OBJECTS BELOW--
+
 var onePike = {
   minCust: 23,
   maxCust: 65,
   avgSale: 6.3,
   avgHourly: function(){
-    return Math.floor(Math.random((this.maxCust - this.minCust + 1)) + min) * (avgSale);
+    return Math.floor(Math.random() * (((this.maxCust - this.minCust + 1) + this.minCust)) * this.avgSale);
   },
 };
 
@@ -39,7 +65,7 @@ var seaTac = {
   maxCust: 24,
   avgSale: 1.2,
   avgHourly: function(){
-    return Math.floor(Math.random((this.maxCust - this.minCust + 1)) + min) * (avgSale);
+    return Math.floor(Math.random() * (((this.maxCust - this.minCust + 1) + this.minCust)) * this.avgSale);
   }
 };
 
@@ -48,7 +74,7 @@ var seaCenter = {
   maxCust: 38,
   avgSale: 3.7,
   avgHourly: function(){
-    return Math.floor(Math.random((this.maxCust - this.minCust + 1)) + min) * (avgSale);
+    return Math.floor(Math.random() * (((this.maxCust - this.minCust + 1) + this.minCust)) * this.avgSale);
   }
 };
 
@@ -57,7 +83,7 @@ var capHill = {
   maxCust: 38,
   avgSale: 2.3,
   avgHourly: function(){
-    return Math.floor(Math.random((this.maxCust - this.minCust + 1)) + min) * (avgSale);
+    return Math.floor(Math.random() * (((this.maxCust - this.minCust + 1) + this.minCust)) * this.avgSale);
   }
 };
 
@@ -66,6 +92,39 @@ var alki = {
   maxCust: 16,
   avgSale: 4.6,
   avgHourly: function(){
-    return Math.floor(Math.random((this.maxCust - this.minCust + 1)) + min) * (avgSale);
+    return Math.floor(Math.random() * (((this.maxCust - this.minCust + 1) + this.minCust)) * this.avgSale);
   }
 };
+
+var avgCookies = [onePike.avgHourly(), seaTac.avgHourly(), seaCenter.avgHourly(), capHill.avgHourly(), alki.avgHourly()];
+
+// loopAvgCookies();
+
+// for(var z = 0; 0 < stores.list; z++){
+//   var eListTitle = document.createElement('h1');
+//   console.log(loopstore());
+//   eListTitle.textContent = loopStore();
+//   ('body').appendChild(eListTitle);
+//
+// }
+function loopTime(){
+  for(var i = 0; i < timeDay.length; i++){
+    console.log(avgCookies[i]);
+    console.log(timeDay[i]);
+  }
+};
+
+for(var h = 0; h < timeDay.length; h++){
+  var eListItem = document.createElement('li');
+  console.log(aDay(onePike));
+  eListItem.textContent = loopTime() + ': ' + aDay(onePike);
+  eListStart.appendChild(eListItem);
+}
+
+// storesList();
+// aDay(seaTac);
+// aDay(seaTac);
+// aDay(capHill);
+// aDay(alki);
+
+//DEMOING PRINTING TO PAGE
